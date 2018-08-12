8f0c38e7597a7255bc7c072cbbfcf21d8bb63112
michele lafleur <mlafleurr@gmail.com>
2018-08-12 22:09:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop (array) {
   for (let i = 0; i < 25; i++) {
-    console.log("I am " + i + " strange loops.");
+    if (i==1) {
+      console.log("I am 1 strang loop");
+    else
+    
+    }
   }
 }
